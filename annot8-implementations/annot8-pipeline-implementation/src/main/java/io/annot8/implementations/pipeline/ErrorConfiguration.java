11c88653b3019667d833271e7f3bff6516907744
James Baker <jdbaker@dstl.gov.uk>
2021-03-29 10:54:06 +0100

    Testing

--- annot8-implementations/annot8-pipeline-implementation/src/main/java/io/annot8/implementations/pipeline/ErrorConfiguration.java
+++ annot8-implementations/annot8-pipeline-implementation/src/main/java/io/annot8/implementations/pipeline/ErrorConfiguration.java
@@ -1,6 +1,8 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.implementations.pipeline;
 
+import java.util.Objects;
+
 public class ErrorConfiguration {
   private OnSourceError onSourceError = OnSourceError.REMOVE_SOURCE;
   private OnProcessingError onItemError = OnProcessingError.DISCARD_ITEM;
@@ -28,6 +30,33 @@ public class ErrorConfiguration {
 
   public void setOnProcessorError(OnProcessingError onProcessorError) {
     this.onProcessorError = onProcessorError;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ErrorConfiguration that = (ErrorConfiguration) o;
+    return onSourceError == that.onSourceError
+        && onItemError == that.onItemError
+        && onProcessorError == that.onProcessorError;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(onSourceError, onItemError, onProcessorError);
+  }
+
+  @Override
+  public String toString() {
+    return "ErrorConfiguration{"
+        + "onSourceError="
+        + onSourceError
+        + ", onItemError="
+        + onItemError
+        + ", onProcessorError="
+        + onProcessorError
+        + '}';
   }
 
   public enum OnSourceError {
